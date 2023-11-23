74319c26b4032c05b5bc312a441fe1bdcb237a7f
이민구 <escapeweedy@gmail.com>
2023-11-23 16:27:06 +0900

    feat: 11번 문제 풀이

---
+++ src/test/ex11.js
@@ -0,0 +1,13 @@
+// VS Code에서 JSDoc을 사용하여 코드의 API 문서를 생성하면 코드의 이해와 공유가 쉬워집니다.
+
+/**
+ * 더하기 함수
+ *
+ * @param {number} x 첫 번째 매개변수
+ * @param {number} y 두 번째 매개변수
+ * @returns {number} 두 매개변수의 합
+ */
+
+function add(x, y) {
+  return x + y;
+}
