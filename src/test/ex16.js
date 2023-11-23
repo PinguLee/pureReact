727f394dfca79c9c44baa029022f158abc73bad2
이민구 <escapeweedy@gmail.com>
2023-11-23 16:56:57 +0900

    feat: 16번 문제 풀이

--- src/test/ex16.js
+++ src/test/ex16.js
@@ -1,1 +1,25 @@
-console.log('Hello, World!');
+let a = 42;
+console.log(typeof a);  // number
+
+let b = "Hello, World!";
+console.log(typeof b);  // string
+
+let c = true;
+console.log(typeof c);  // boolean
+
+let d = { key: "value" };
+console.log(typeof d);  // object
+
+let e = [1, 2, 3];
+console.log(typeof e);  // object"
+
+let f = function() {
+    console.log("Function");
+};
+console.log(typeof f);  // function
+
+let g;
+console.log(typeof g);  // undefined
+
+let h = null;
+console.log(typeof h);  // object
