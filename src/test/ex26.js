d004f0a4f0bcde85432356253410338391a782fe
이민구 <escapeweedy@gmail.com>
2023-11-26 18:05:36 +0900

    feat: 26번 문제 풀이

--- src/test/ex26.js
+++ src/test/ex26.js
@@ -1,1 +1,12 @@
-console.log('Hello, World!');
+let value = 1;
+
+switch (value) {
+  case 1:
+    console.log("1");
+    break;
+  case 2:
+    console.log("2");
+    break;
+  default:
+    console.log("1, 2 아님");
+}
