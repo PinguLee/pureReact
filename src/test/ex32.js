3a9f8d9a7129473ecf3a9ebbca4f005bb04cca5d
이민구 <escapeweedy@gmail.com>
2023-11-26 18:09:27 +0900

    feat: 32번 문제 풀이

--- src/test/ex32.js
+++ src/test/ex32.js
@@ -1,1 +1,10 @@
-console.log('Hello, World!');
+const object = {
+  address: "대덕대로",
+  number: 182,
+};
+
+const address = object.address;
+console.log(address);
+
+const number = object["number"];
+console.log(number);
